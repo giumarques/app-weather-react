cd6ebaf4d9537ea7cd33df029154cc312dd313e9
Giuliana Neri <giulianam.neri@gmail.com>
2023-08-03 21:49:10 +0100

    added new c

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,39 +1,40 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-    return (
-      <div className="WeatherInfo">
-        <h1>{props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={props.data.date} />
-          </li>
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
 
-          <li className="text-capitalize">{props.data.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="d-flex">
-              <div>
-                <WeatherIcon code={props.data.icon} size={52} />
-              </div>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
 
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+            <div>
+              {/* WeatherTemperature component */}
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Preciptitation: 15%</li>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
